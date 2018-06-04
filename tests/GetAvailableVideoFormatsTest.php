14540f9ddde46c174710eb2090ef55e2c98fc514
Gowtham Alaguraj <gowtham25alaguraj@gmail.com>
2018-06-04 11:00:47 +0530

    used GuzzleHttp new Request class

--- tests/GetAvailableVideoFormatsTest.php
+++ tests/GetAvailableVideoFormatsTest.php
@@ -12,7 +12,7 @@ class GetAvailableVideoFormatsTest extends TestCase
 		
 		$body['url'] = "http://www.hotstar.com/tv/chinnathambi/15301/chinnathambi-yearns-for-nandini/1100003795";
 		$url = "http://hotstar-test1.herokuapp.com/getAvailableVideoFormats.php";
-		$response = $client->createRequest("POST", $url, ['body'=>$body]);
+		$response = new \GuzzleHttp\Psr7\Request("POST", $url, ['body'=>$body]);
 		$response = $client->send($response);
 		
 		var_dump($response);
