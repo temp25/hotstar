e32b1ac98c711cf45688fa4556433ccfd9f2ea2b
Gowtham Alaguraj <gowtham25alaguraj@gmail.com>
2018-06-04 10:45:57 +0530

    used GuzzleHttp library

--- tests/GetAvailableVideoFormatsTest.php
+++ tests/GetAvailableVideoFormatsTest.php
@@ -3,21 +3,21 @@ use PHPUnit\Framework\TestCase;
 
 class GetAvailableVideoFormatsTest extends TestCase
 {
-	protected $client;
-
-    protected function setUp()
-    {
-        $this->client = new GuzzleHttp\Client([
-            'base_uri' => ''
-        ]);
-    }
 	
     public function testGetFormats()
     {
+		
+		// create our http client (Guzzle)
+		$client = new Guzzle\Client('http://hotstar-test1.herokuapp.com', array(
+			'request.options' => array(
+				'exceptions' => false,
+			)
+		));
+		
 		$data = array();
 		$data['url'] = "http://www.hotstar.com/tv/chinnathambi/15301/chinnathambi-yearns-for-nandini/1100003795";
 		
-		$request = $client->post('http://hotstar-test1.herokuapp.com/getAvailableVideoFormats.php', null, json_encode($data));
+		$request = $client->post('/getAvailableVideoFormats.php', null, json_encode($data));
 		$response = $request->send();
 
 		var_dump($response);
