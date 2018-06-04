eeccbda65417ede71f02e7acad72e018c5ee6784
Gowtham Alaguraj <gowtham25alaguraj@gmail.com>
2018-06-04 10:17:23 +0530

    Added unit tests for getAvailableVideoFormats.php

---
+++ tests/GetAvailableVideoFormatsTest.php
@@ -0,0 +1,29 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+class GetAvailableVideoFormatsTest extends PHPUnit_Framework_TestCase
+{
+	protected $client;
+
+    protected function setUp()
+    {
+        $this->client = new Client([
+            'base_uri' => ''
+        ]);
+    }
+	
+    public function testGetFormats()
+    {
+		$data = array();
+		$data['url'] = "http://www.hotstar.com/tv/chinnathambi/15301/chinnathambi-yearns-for-nandini/1100003795";
+		
+		$request = $client->post('http://hotstar-test1.herokuapp.com/getAvailableVideoFormats.php', null, json_encode($data));
+		$response = $request->send();
+
+		var_dump($response);
+		
+		$this->assertEquals(200, $response->getStatusCode());
+		
+    }
+}
+?>
