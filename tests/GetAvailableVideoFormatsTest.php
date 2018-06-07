22b2e08b013ed1693fb74521088a2d525b7195c0
Gowtham Alaguraj <gowtham25alaguraj@gmail.com>
2018-06-07 11:29:19 +0530

    Reverted back to PHP in-built POST request

--- tests/GetAvailableVideoFormatsTest.php
+++ tests/GetAvailableVideoFormatsTest.php
@@ -15,21 +15,26 @@ class GetAvailableVideoFormatsTest extends TestCase
 		$response = new \GuzzleHttp\Psr7\Request("POST", $url, ['body'=>$body]);
 		$response = $client->send($response);
 		
-		$content = (string) $response->getBody();
-		
-		echo "Response : ".$content;
-		var_dump($content);
-		
 		$this->assertEquals(200, $response->getStatusCode());
 		
-		$client = new \GuzzleHttp\Client();
-        $response = $client->post('http://hotstar-test1.herokuapp.com/getAvailableVideoFormats.php', [
-            'query' => ['url' => 'http://www.hotstar.com/tv/chinnathambi/15301/chinnathambi-yearns-for-nandini/1100003795']
-        ]);
 		
-		$body = $response->getBody();
-		
-		var_dump(json_decode($body));
+		$url = 'http://hotstar-test1.herokuapp.com/getAvailableVideoFormats.php';
+		$data = array('url' => 'http://www.hotstar.com/tv/chinnathambi/15301/chinnathambi-yearns-for-nandini/1100003795');
+
+		// use key 'http' even if you send the request to https://...
+		$options = array(
+			'http' => array(
+				'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+				'method'  => 'POST',
+				'content' => http_build_query($data)
+			)
+		);
+		$context  = stream_context_create($options);
+		$result = file_get_contents($url, false, $context);
+		if ($result === FALSE) { /* Handle error */ }
+
+		echo PHP_EOL."Result : ".PHP_EOL;
+		var_dump($result);
 		
     }
 }
