1541e829b36af118180f2821044fcbf8ddacff3e
Gowtham Alaguraj <gowtham25alaguraj@gmail.com>
2018-06-07 12:33:47 +0530

    Fixed jUnit test

--- tests/GetAvailableVideoFormatsTest.php
+++ tests/GetAvailableVideoFormatsTest.php
@@ -62,30 +62,13 @@ class GetAvailableVideoFormatsTest extends TestCase
 		
 		$response = json_decode($result, true);
 		
-		$expected = array();
-		$expected['status'] = "true";
-		$expected['source'] = "api";
-		$expected['videoId'] = "1000093817";
-		$expected['episodeNumber'] = 1;
-		$expected['title'] = "Tori, a Pampered Child";
-		$expected['description'] = "Starting tonight, Khoka Babu is about a happy-go-lucky girl, Tori. The only daughter of industrialist Rajsekhar, she is pampered by her father. Tori's mother, Anuradha is worried about her carefree nature. Her father fixes her engagement with Preet, but Anuradha is unhappy with the alliance.";
-		$expected['hls-64 - 250x140'] = "https://staragvod2-vh.akamaihd.net/i/videos/jalsha/kb/1/1000093817_,16,54,106,180,400,800,1300,2000,3000,4500,_STAR.mp4.csmil/index_1_av.m3u8?null=0&id=AgDUL+9aX2T8HljTGFuqyb4TWT5w3SX2sFRnuzSETVywc+4lQcg5crMCw8Vqa%2fhuoOOXFvsGhmMhkw%3d%3d";
-		$expected['hls-121 - 320x180'] = "https://staragvod2-vh.akamaihd.net/i/videos/jalsha/kb/1/1000093817_,16,54,106,180,400,800,1300,2000,3000,4500,_STAR.mp4.csmil/index_2_av.m3u8?null=0&id=AgDUL+9aX2T8HljTGFuqyb4TWT5w3SX2sFRnuzSETVywc+4lQcg5crMCw8Vqa%2fhuoOOXFvsGhmMhkw%3d%3d";
-		$expected['hls-241 - 320x180'] = "https://staragvod2-vh.akamaihd.net/i/videos/jalsha/kb/1/1000093817_,16,54,106,180,400,800,1300,2000,3000,4500,_STAR.mp4.csmil/index_3_av.m3u8?null=0&id=AgDUL+9aX2T8HljTGFuqyb4TWT5w3SX2sFRnuzSETVywc+4lQcg5crMCw8Vqa%2fhuoOOXFvsGhmMhkw%3d%3d";
-		$expected['hls-461 - 416x234'] = "https://staragvod2-vh.akamaihd.net/i/videos/jalsha/kb/1/1000093817_,16,54,106,180,400,800,1300,2000,3000,4500,_STAR.mp4.csmil/index_4_av.m3u8?null=0&id=AgDUL+9aX2T8HljTGFuqyb4TWT5w3SX2sFRnuzSETVywc+4lQcg5crMCw8Vqa%2fhuoOOXFvsGhmMhkw%3d%3d";
-		$expected['hls-861 - 640x360'] = "https://staragvod2-vh.akamaihd.net/i/videos/jalsha/kb/1/1000093817_,16,54,106,180,400,800,1300,2000,3000,4500,_STAR.mp4.csmil/index_5_av.m3u8?null=0&id=AgDUL+9aX2T8HljTGFuqyb4TWT5w3SX2sFRnuzSETVywc+4lQcg5crMCw8Vqa%2fhuoOOXFvsGhmMhkw%3d%3d";
-		$expected['hls-1361 - 720x404'] = "https://staragvod2-vh.akamaihd.net/i/videos/jalsha/kb/1/1000093817_,16,54,106,180,400,800,1300,2000,3000,4500,_STAR.mp4.csmil/index_6_av.m3u8?null=0&id=AgDUL+9aX2T8HljTGFuqyb4TWT5w3SX2sFRnuzSETVywc+4lQcg5crMCw8Vqa%2fhuoOOXFvsGhmMhkw%3d%3d";
-		$expected['hls-2060 - 1280x720'] = "https://staragvod2-vh.akamaihd.net/i/videos/jalsha/kb/1/1000093817_,16,54,106,180,400,800,1300,2000,3000,4500,_STAR.mp4.csmil/index_7_av.m3u8?null=0&id=AgDUL+9aX2T8HljTGFuqyb4TWT5w3SX2sFRnuzSETVywc+4lQcg5crMCw8Vqa%2fhuoOOXFvsGhmMhkw%3d%3d";
-		$expected['hls-3061 - 1600x900'] = "https://staragvod2-vh.akamaihd.net/i/videos/jalsha/kb/1/1000093817_,16,54,106,180,400,800,1300,2000,3000,4500,_STAR.mp4.csmil/index_8_av.m3u8?null=0&id=AgDUL+9aX2T8HljTGFuqyb4TWT5w3SX2sFRnuzSETVywc+4lQcg5crMCw8Vqa%2fhuoOOXFvsGhmMhkw%3d%3d";
-		$expected['hls-4562 - 1920x1080'] = "https://staragvod2-vh.akamaihd.net/i/videos/jalsha/kb/1/1000093817_,16,54,106,180,400,800,1300,2000,3000,4500,_STAR.mp4.csmil/index_9_av.m3u8?null=0&id=AgDUL+9aX2T8HljTGFuqyb4TWT5w3SX2sFRnuzSETVywc+4lQcg5crMCw8Vqa%2fhuoOOXFvsGhmMhkw%3d%3d";
+		$this->assertEquals("true", $response['status']);
+		$this->assertEquals("api", $response['source']);
+		$this->assertEquals("1000093817", $response['videoId']);
+		$this->assertEquals(1, $response['episodeNumber']);
+		$this->assertEquals("Tori, a Pampered Child", $response['title']);
+		$this->assertEquals("Starting tonight, Khoka Babu is about a happy-go-lucky girl, Tori. The only daughter of industrialist Rajsekhar, she is pampered by her father. Tori's mother, Anuradha is worried about her carefree nature. Her father fixes her engagement with Preet, but Anuradha is unhappy with the alliance.", $response['description']);
 		
-		echo PHP_EOL."Expected : ".PHP_EOL;
-		var_dump($expected);
-		
-		echo PHP_EOL.PHP_EOL."Actual : ".PHP_EOL;
-		var_dump($response);
-		
-		$this->assertEquals($expected, $response);
 		
     }
 	
