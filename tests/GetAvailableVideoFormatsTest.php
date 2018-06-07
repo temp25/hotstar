1e562d60835e8e504f084625ffb3da97dcd0bf1c
Gowtham Alaguraj <gowtham25alaguraj@gmail.com>
2018-06-07 11:46:19 +0530

    Modified unit test

--- tests/GetAvailableVideoFormatsTest.php
+++ tests/GetAvailableVideoFormatsTest.php
@@ -6,18 +6,6 @@ class GetAvailableVideoFormatsTest extends TestCase
 	
     public function testGetFormats()
     {
-		
-		// create our http client (Guzzle)
-		$client = new \GuzzleHttp\Client();
-		
-		$body['url'] = "http://www.hotstar.com/tv/chinnathambi/15301/chinnathambi-yearns-for-nandini/1100003795";
-		$url = "http://hotstar-test1.herokuapp.com/getAvailableVideoFormats.php";
-		$response = new \GuzzleHttp\Psr7\Request("POST", $url, ['body'=>$body]);
-		$response = $client->send($response);
-		
-		$this->assertEquals(200, $response->getStatusCode());
-		
-		
 		$url = 'http://hotstar-test1.herokuapp.com/getAvailableVideoFormats.php';
 		$data = array('url' => 'http://www.hotstar.com/tv/chinnathambi/15301/chinnathambi-yearns-for-nandini/1100003795');
 
@@ -31,10 +19,26 @@ class GetAvailableVideoFormatsTest extends TestCase
 		);
 		$context  = stream_context_create($options);
 		$result = file_get_contents($url, false, $context);
-		if ($result === FALSE) { /* Handle error */ }
-
-		echo PHP_EOL."Result : ".PHP_EOL;
-		var_dump($result);
+		
+		$this->assertTrue($result !== FALSE);
+		
+		$response = json_decode($result, true);
+		
+		$expected = array();
+		$expected['status'] = "true";
+		$expected['source'] = "ydl";
+		$expected['videoId'] = "1100003795";
+		$expected['playlistId'] = 29;
+		$expected['hls-121'] = "320x180";
+		$expected['ls-241'] = "320x180";
+		$expected['hls-461'] = "416x234";
+		$expected['hls-861'] = "640x360";
+		$expected['hls-1362'] = "720x404";
+		$expected['hls-2063'] = "1280x720";
+		$expected['hls-3192'] = "1600x900";
+		$expected['hls-4694'] = "1920x1080";
+		
+		$this->assertEquals($expected, $response);
 		
     }
 }
