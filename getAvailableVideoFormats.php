c649604bba5271874aaf1743c2a9b3f3671069b5
Gowtham Alaguraj <gowtham25alaguraj@gmail.com>
2018-06-22 09:55:40 +0530

    Added include keyword to import the class from src folder

--- getAvailableVideoFormats.php
+++ getAvailableVideoFormats.php
@@ -1,12 +1,14 @@
 <?php
 	
+	include('src/VideoFormats.php');
+	
 	if(!isset($_POST['url'])){
 		die("Error no POST url data given");
 	}
 
 	$videoUrl = $_POST['url'];
 
-	$videoFormats = new src/VideoFormats($videoUrl);
+	$videoFormats = new VideoFormats($videoUrl);
 
 	$formats = $videoFormats->isAvailable();
 
