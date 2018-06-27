6aeb928348af05c0d09512ae1f3fb46786fb381c
Gowtham Alaguraj <gowtham25alaguraj@gmail.com>
2018-06-27 09:21:42 +0530

    Added code for script execution after sending http response

--- generateVideo.php
+++ generateVideo.php
@@ -7,19 +7,7 @@
 	   
 		$src = $_POST['src'];
 		
-		ob_end_clean();
-		header("Connection: close\r\n");
-		header("Content-Encoding: none\r\n");
-		ignore_user_abort(true);
-		// optional
-		
-		ob_start();
-		echo('Processing video');
-		$size=ob_get_length();
-		header("Content-Length: $size");
-		ob_end_flush();    // Strange behaviour, will not work
-		flush();           // Unless both are called !
-		ob_end_clean();
+		respondOK();
 		
 		exec("chmod a+rx youtube-dl");
 		exec("tar xvzf files.tar.gz");
@@ -111,6 +99,51 @@
 		
 		sendProgressToClient($progress, $ipAddr_userAgent);
 		
+	}
+	
+	/**
+	 * Respond 200 OK with an optional
+	 * This is used to return an acknowledgement response indicating that the request has been accepted and then the script can continue processing
+	 *
+	 * @param null $text
+	 */
+	public function respondOK($text = null)
+	{
+		// check if fastcgi_finish_request is callable
+		if (is_callable('fastcgi_finish_request')) {
+			if ($text !== null) {
+				echo $text;
+			}
+			/*
+			 * http://stackoverflow.com/a/38918192
+			 * This works in Nginx but the next approach not
+			 */
+			session_write_close();
+			fastcgi_finish_request();
+	 
+			return;
+		}
+	 
+		ignore_user_abort(true);
+	 
+		ob_start();
+	 
+		if ($text !== null) {
+			echo $text;
+		}
+	 
+		$serverProtocol = filter_input(INPUT_SERVER, 'SERVER_PROTOCOL', FILTER_SANITIZE_STRING);
+		header($serverProtocol . ' 200 OK');
+		// Disable compression (in case content length is compressed).
+		header('Content-Encoding: none');
+		header('Content-Length: ' . ob_get_length());
+	 
+		// Close the connection.
+		header('Connection: close');
+	 
+		ob_end_flush();
+		ob_flush();
+		flush();
 	}
 	
 	
