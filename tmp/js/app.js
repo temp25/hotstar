3b395d9d56e46979b4400253f56bcef66b0a92af
Gowtham Alaguraj <gowtham25alaguraj@gmail.com>
2018-12-20 17:00:29 +0530

    Debug #39

--- tmp/js/app.js
+++ tmp/js/app.js
@@ -35,17 +35,11 @@ app.controller("Controller1", function($scope, $state, $http, $timeout) {
 	.then(function(response) {
 		//success
 		responsePostData = response.data;
-		//$scope.videoFormats = response.data.availableFormats;
-		//SharedLoc.put('container1', $scope);
-		console.log("status : "+response.status+" data : "+response.data);
-		//$location.path("/route2");
 		$state.go("route2", {videoFormats: response.data.availableFormats});
 	},
 	function(response) { // optional
 		// failed
-		responsePostData = response.data;
 		console.log("status : "+response.status+" data : "+response.data);
-		//$location.path("/route2");
     });
     
     
@@ -54,28 +48,8 @@ app.controller("Controller1", function($scope, $state, $http, $timeout) {
 
 
 app.controller("Controller2", function($scope, $stateParams, $http, $timeout) {
-	//SharedLoc.get('container1');
-	vidFormat = $stateParams.videoFormats;
-	//console.log("typeof vidFormat : "+(typeof vidFormat)+"\nvidFormat:\n"+vidFormat);
-	//vidFormat.forEach((item) => {
-		//console.log("item.id: "+item.id+"__item.format_code: "+item.format_code+"__item.format_resolution: "+item.format_resolution);
-	//});
+	$scope.videoFormats = $stateParams.videoFormats;
 	
-	/* videoFormats = JSON.parse(JSON.stringify(vidFormat), function(k, v){	
-		if(k === "id"){
-				if(isNaN(v))
-					return v;
-				else
-					return parseInt(v, 10);
-		}
-		return v;
-	});
-	console.log(videoFormats); */
-	
-	//var jsonStringify = JSON.stringify(vidFormat);
-	//console.log("jsonStringify : "+jsonStringify+"\n\n");
-	
-	$scope.videoFormats = vidFormat;
 	$scope.onFormatChange = function() {
 		if ($scope.formats != null) {
 		  var element = document.getElementById("defFormat");
@@ -87,5 +61,4 @@ app.controller("Controller2", function($scope, $stateParams, $http, $timeout) {
 	$scope.generateVideo = function(){
 		console.log("selectedFormat : "+$scope.selectedFormat);
 	}
-  
 });
