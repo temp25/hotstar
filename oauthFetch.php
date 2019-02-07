b59fcc312cf93b1a4fbd3ddef2fb78bf05a7d533
Gowtham Alaguraj <gowtham25alaguraj@outlook.com>
2019-02-07 11:42:52 +0530

    uncommented redirect to redirectUri page

--- oauthFetch.php
+++ oauthFetch.php
@@ -9,6 +9,6 @@
 	$client->setRedirectUri('https://hotstar-test1.herokuapp.com/redirect.php');
 	$auth_url = $client->createAuthUrl();
 	$sanitized_auth_url = filter_var($auth_url, FILTER_SANITIZE_URL);
-	echo $sanitized_auth_url;
-	//header('Location: ' . $sanitized_auth_url);
+	//echo $sanitized_auth_url;
+	header('Location: ' . $sanitized_auth_url);
 ?>
