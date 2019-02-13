6ac5e6c732fa6bac49c5a5d80a2bd55f28d81cde
Gowtham Alaguraj <gowtham25alaguraj@outlook.com>
2019-02-13 12:10:39 +0530

    Added initial commit for upload to onedrive

---
+++ OneDriveConfig.php
@@ -0,0 +1,20 @@
+<?php
+// config.php
+
+return [
+    /**
+     * Your OneDrive client ID.
+     */
+    'ONEDRIVE_CLIENT_ID' => 'd81bf1c4-16c5-4852-87ba-1cd43f752ec0',
+
+    /**
+     * Your OneDrive client secret.
+     */
+    'ONEDRIVE_CLIENT_SECRET' => 'kqoabzHO567]uLIAZD22[-!',
+
+    /**
+     * Your OneDrive redirect URI.
+     */
+    'ONEDRIVE_REDIRECT_URI' => 'https://hotstar-test1.herokuapp.com/onedrive_redirect.html',
+];
+?>
