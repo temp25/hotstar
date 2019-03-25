e45ca49db3bd3c2484f2d584da445d960a7aca89
Gowtham Alaguraj <gowtham25alaguraj@gmail.com>
2019-03-25 09:26:32 +0530

    Refactored code

--- downloadVideo.php
+++ downloadVideo.php
@@ -1,24 +1,41 @@
 <?php
 
 if (!isset($_GET['videoId'])) {
-    die('Invalid invocation of file');
+    die("Invalid invocation of file");
 }
 
-$filename = $_GET['videoId'] . ".zip";
+$filename = $_GET["videoId"] . ".zip";
 $absoluteFilePath = getcwd() . DIRECTORY_SEPARATOR . $filename;
 
-// http headers for zip downloads 
-
-header("Pragma: public");
-header("Expires: 0");
-header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
-header("Cache-Control: public");
-header("Content-Description: File Transfer");
-header("Content-type: application/zip, application/octet-stream");
-header("Content-Disposition: attachment; filename=\"" . $filename . "\"");
-header("Content-Transfer-Encoding: binary");
-header("Content-Length: " . filesize($absoluteFilePath));
-ob_end_flush();
-@readfile($absoluteFilePath);
+if(is_file($absoluteFilePath)){
+    // http headers for zip downloads
+    
+    header("Pragma: public");
+    header("Expires: 0");
+    header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
+    header("Cache-Control: public");
+    header("Content-Description: File Transfer");
+    header("Content-type: application/zip, application/octet-stream");
+    header("Content-Disposition: attachment; filename=\"" . $filename . "\"");
+    header("Content-Transfer-Encoding: binary");
+    header("Content-Length: " . filesize($absoluteFilePath));
+    
+    $file = fopen($absoluteFilePath, "r");
+    
+    while(!feof($file)) {
+        print(fread($file, 8 * 1024)); // 8KiB
+        ob_flush();
+        flush();
+        if(connection_status()!=0){
+            fclose($file);
+            exit;
+        }
+    }
+    
+    fclose($file);
+    exit;
+} else {
+    die("File $filename doesn't exist...");
+}
 
 ?>
