646e20a67ea34d04f422f7036eb2f1a004ad4a4a
Gowtham Alaguraj <gowtham25alaguraj@gmail.com>
2019-03-29 10:13:28 +0530

    Added files for Onedrive upload

---
+++ oneDriveConfig.php
@@ -0,0 +1,19 @@
+<?php
+// config.php
+
+return [
+    /**
+     * Your OneDrive client ID.
+     */
+    'ONEDRIVE_CLIENT_ID' => 'd81bf1c4-16c5-4852-87ba-1cd43f752ec0',
+
+    /**
+     * Your OneDrive client secret.
+     */
+    'ONEDRIVE_CLIENT_SECRET' => "+((..(.L{_&h;!Z-ju0+{&!",
+
+    /**
+     * Your OneDrive redirect URI.
+     */
+    'ONEDRIVE_REDIRECT_URI' => "https://hotstar-test1.herokuapp.com/redirect_onedrive.php",
+];
